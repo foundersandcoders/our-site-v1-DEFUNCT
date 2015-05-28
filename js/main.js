90448d929d9df3236fcc1cc8904b77ebebbad8c3
Izaak Sofer <izaakrogan@googlemail.com>
2015-05-28 13:25:01 +0200

    nav js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,47 @@ function init() {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             navbar = document.getElementById("navbar");
         if (distanceY > 300) {
-            navbar.className += " smaller";
+            navbar.className += " small-nav";
         } else {
-            if(navbar.className.match(/(?:^|\s)smaller(?!\S)/)) {
-            navbar.className = navbar.className.replace(/(?:^|\s)smaller(?!\S)/g ,'')
+            if(navbar.className.match(/(?:^|\s)small-nav(?!\S)/)) {
+            navbar.className = navbar.className.replace(/(?:^|\s)small-nav(?!\S)/g ,'')
             }
         }
     });
 }
 
+function init2() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            elements = document.getElementsByClassName('nav-dd');
+        if (distanceY > 300) {
+            for (var i=elements.length; i--;) {
+                elements[i].classList.add("small-dd");
+            }
+        } 
+        else {
+            if(elements[0].className.match(/(?:^|\s)small-dd(?!\S)/)) {
+
+                for (var i=elements.length; i--;) {
+                    elements[i].classList.remove("small-dd");
+                }
+
+            // elements.className = elements.className.replace(/(?:^|\s)small-dd(?!\S)/g ,'')
+            }
+        }
+    });
+}
+
+function removeClass( classname, element ) {
+    var cn = element.className;
+    var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
+    cn = cn.replace( rxp, '' );
+    element.className = cn;
+}
+
 function start() {
-    init();
+    init();init2();
+
 }
 
 window.onload = start;
