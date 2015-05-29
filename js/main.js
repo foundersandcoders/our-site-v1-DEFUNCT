d5db29b636f605e564797df1ac837cd0421af777
Izaak Sofer <izaakrogan@googlemail.com>
2015-05-29 10:26:17 +0200

    nav scrolling js

--- js/main.js
+++ js/main.js
@@ -74,3 +74,70 @@ function changeText(one, two){
         document.getElementById('client-quote').style.opacity = 1;
     }, 1000);    
 }
+
+function go(x) {
+    var pos = document.getElementById(x).getBoundingClientRect();
+    window.scroll(0, pos.top);
+}
+
+function sc(){
+    var element = document.getElementById("portfolio");
+    element.scrollIntoView();
+}
+
+
+function currentYPosition() {
+    if (self.pageYOffset) return self.pageYOffset;
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+
+function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+}
+
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
+
+function nav_scroll(page, anchor) {
+    if (window.location.pathname != page) {
+        location = page+'#'+anchor
+    }
+    //Stop page scrolling before load
+    setTimeout(function() {
+    smoothScroll(anchor);
+    }, 1);
+    console.log(window.location.pathname);
+}
+
+//alternatively location = page + special: anchor
+//onload smoothScroll(anchor)
